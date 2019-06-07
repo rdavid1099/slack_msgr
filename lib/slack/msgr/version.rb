fbe6ddccb0f74e980d7a202d80016da2d2edb7d0
Ryan Workman <rdavid1099@gmail.com>
2019-06-07 10:49:52 -0700

    Rework file loading and names to get first test to pass

--- lib/slack/msgr/version.rb
+++
@@ -1,5 +0,0 @@
-module Slack
-  module Msgr
-    VERSION = "0.1.0"
-  end
-end
---
+++ spec/unit/slack_msgr_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe SlackMsgr do
+  it 'has a version number of 0.1.0' do
+    expect(SlackMsgr::VERSION).to eq('0.1.0')
+  end
+end
