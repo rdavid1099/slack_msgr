23b44660dcf3d8493e1e97011ad159c070504880
Ryan Workman <rdavid1099@gmail.com>
2019-06-10 16:19:05 -0700

    Add test for Configuration#clear

--- spec/unit/configuration_spec.rb
+++ spec/unit/configuration_spec.rb
@@ -1,23 +1,35 @@
 RSpec.describe SlackMsgr::Configuration do
+  let(:config) do
+    SlackMsgr::Configuration.new(
+      verification_token: 'foo',
+      client_secret:      'bar',
+      signing_secret:     'baz'
+    )
+  end
+
   describe '#initialize' do
     it 'can be initialized without parameters' do
-      config = SlackMsgr::Configuration.new
+      blank_config = SlackMsgr::Configuration.new
+
+      expect(blank_config.verification_token).to eq(nil)
+      expect(blank_config.client_secret).to eq(nil)
+      expect(blank_config.signing_secret).to eq(nil)
+    end
+
+    it 'can be initialized with parameters' do
+      expect(config.verification_token).to eq('foo')
+      expect(config.client_secret).to eq('bar')
+      expect(config.signing_secret).to eq('baz')
+    end
+  end
+
+  describe '#clear!' do
+    it 'permanently clears all configuration variables' do
+      config.clear!
 
       expect(config.verification_token).to eq(nil)
       expect(config.client_secret).to eq(nil)
       expect(config.signing_secret).to eq(nil)
-    end
-
-    it 'can be initialized with parameters' do
-      config = SlackMsgr::Configuration.new(
-        verification_token: 'foo',
-        client_secret:      'bar',
-        signing_secret:     'baz'
-      )
-
-      expect(config.verification_token).to eq('foo')
-      expect(config.client_secret).to eq('bar')
-      expect(config.signing_secret).to eq('baz')
     end
   end
 end
