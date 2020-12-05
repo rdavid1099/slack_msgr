5009e9a3a84e50e54422a3c316842e51f12d407f
Ryan Workman <rdavid1099@users.noreply.github.com>
2020-12-05 15:26:05 -0800

    Bump version

--- spec/unit/slack_msgr_spec.rb
+++ spec/unit/slack_msgr_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe SlackMsgr do
-  it 'has a version number of 0.0.6' do
-    expect(SlackMsgr::VERSION).to eq('0.0.6')
+  it 'has a version number of 0.0.7' do
+    expect(SlackMsgr::VERSION).to eq('0.0.7')
   end
 end
